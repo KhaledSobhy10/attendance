cf23d7885b31b1adb5db599799f71e38876abfd7
khaled-sobhy <khaledloda696@gmail.com>
2024-07-23 22:31:51 +0300

    Fix : shifting layout when toast appear .

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import { AttendanceProvider } from "./context/AttendanceContext";
 import Actions from "./components/Actions";
 
 function App() {
-
   return (
-    <AttendanceProvider>
-      <main className="container lg:p-4 p-2 text-center flex flex-col gap-4">
-        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight">
-          مدير الوقت
-        </h1>
-        <Toaster />
-        <Actions/>
-        <AttendanceTable />
-      </main>
-    </AttendanceProvider>
+    <>
+      <Toaster />
+      <AttendanceProvider>
+        <main className="container lg:p-4 p-2 text-center flex flex-col gap-4">
+          <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight">
+            مدير الوقت
+          </h1>
+          <Actions />
+          <AttendanceTable />
+        </main>
+      </AttendanceProvider>
+    </>
   );
 }
 
